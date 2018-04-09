9467cf56ef44c8d34df3a32403204dbd50aa3966
Henry Heng <siekhaiheng@gmail.com>
2018-04-09 21:41:41 +0700

    Fix get category

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -18,7 +18,7 @@ class CategoryResource extends Resource
         return [
             'categoryName'      => $this->category_name,
             'categoryNameKhmer' => $this->category_name_khmer,
-            'categoryImage'     => trim($this->category_image) ? asset(env('IMAGE_PATH')).'/'.$this->category_image : "",
+            'categoryImage'     => trim($this->category_image) ? asset(env('CATEGORY_PATH')).'/'.$this->category_image : "",
             'categoryParent'    => $this->when($this->parentCategory, new CategoryResource($this->parentCategory)),
             'categoryChildren'  => $this->when($this->categoryChildren, $this->categoryChildren),
             'productCount'      => $this->when($this->parentCategory, $this->posts_count)
